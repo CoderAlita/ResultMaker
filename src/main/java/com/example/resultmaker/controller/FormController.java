3c481852512bd79360e2a794f76be7fa37d86695
CoderAlita <priyankavisheshwar1018@gmail.com>
2024-06-20 00:00:24 +0530

    add form

--- src/main/java/com/example/resultmaker/controller/FormController.java
+++ src/main/java/com/example/resultmaker/controller/FormController.java
@@ -1,14 +1,26 @@
 package com.example.resultmaker.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class FormController {
 
 	@GetMapping("/form")
 	
-	public String form() {
+	public String form(Model model) {
+		model.addAttribute("University", "Mumbai");
 		return "form";
+	}
+	
+	@PostMapping("/result")
+	public String result(@ModelAttribute("University") String Result, Model model) {
+		
+		model.addAttribute("name", Result);
+		System.out.println(Result);
+		return "result";
 	}
 }
